7a4347948ae942641bd0d6aa2a58ef611038b392
GOODFARMS-ENG <berteng90@gmail.com>
2022-06-20 09:28:58 +0800

    Configured basic logic

---
+++ script.js
@@ -0,0 +1,20 @@
+const playerSelection = 'rock';
+const computerSelection = computerPlay;
+console.log(playRound(playerSelection, computerSelection))
+
+
+function computerPlay() {
+    const rock = 'rock';
+    const scissors = 'scissors';
+    const paper = 'paper';
+}
+
+function playRound(playerSelection, computerSelection) {
+    for (i = 1; i <= 5; i++) {
+        prompt("Enter your Hand!!!")
+    }
+}
+
+function game() {
+    playRound()
+}
