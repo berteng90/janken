87cac6a5ddab9332be3c8d308981919d6534077d
GOODFARMS-ENG <berteng90@gmail.com>
2022-07-03 14:45:44 +0800

    Finished TheOdinProject Rock Paper Scissors with UI

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
+const player = document.getElementById('player');
+const computer = document.getElementById('computer');
+const update = document.getElementById('round');
+const playerNewHand = document.getElementById('player img');
+const computerNewHand = document.getElementById('computer img');
+const info = document.getElementById('info-result');
+const infoGraphic = document.getElementById('info-graphic');
+const round = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE', 'TEN', 'ELEVEN',
+    'TWELVE', 'THIRTEEN', 'FOURTEEN', 'FIFTEEN', 'SIXTEEN', 'SEVENTEEN', 'EIGHTEEN', 'NINETEEN', 'TWENTY'];
+let count = 0;
+let playerScore = 0;
+let computerScore = 0;
+//Initialize Player Hand
+function playerHand() {
+    const playerHand = document.querySelectorAll('button');
+    playerHand.forEach(button => {
+        button.addEventListener('click', function () {
+            playRound(button.id)
+        })
+    })
+}
+playerHand();
 
 
-
-function playerPlay() {
-   let handOne=document.getElementById('rock');
-   let handTwo=document.getElementById('paper');
-   let handThree=document.getElementById('scissors');
-
-   if (handOne.onclick, ()=>{
-   }){
-    return 'rock';
-   }else if (handTwo.onclick,()=>{
-
-   }){
-    return 'paper';  
-   }else if (handThree.onclick,()=>{
-
-    }){
-    return 'scissors';  
-    }
-    else{
-
-
-    }
-
+//Reset Game
+function restart() {
+    playerNewHand.src = './icons/questionmark.png';
+    computerNewHand.src = './icons/questionmark.png';
+    computer.textContent = 'Computer : 0';
+    player.textContent = 'Player : 0';
+    playerScore = 0;
+    computerScore = 0;
+    update.style.visibility = 'hidden';
+    count = 0;
+    infoGraphic.style.visibility = 'hidden';
 }
 
 //Generate random Computer Hand
@@ -34,46 +42,51 @@ function computerPlay() {
     return (computerCurrentHand);
 }
 
-//Play a Round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection) {
-    
-    const playerScore = 0;
-    const computerScore = 0;
-    let computer=document.getElementById('computer').textContent;
-    let player=document.getElementById('player').textContent;
-        if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerScore++;
-            computer.textContent=`Computer : ${computerScore}`;
+//Play a Round
+function playRound(playerSelection) {
+    info.style.visibility = 'hidden';
+    let computerSelection = computerPlay();
+    infoGraphic.style.visibility = 'visible';
+    playerNewHand.src = `./icons/${playerSelection}.png`;
+    computerNewHand.src = `./icons/${computerSelection}.png`;
+    update.style.visibility = 'visible';
+    if (playerScore !== 5 && computerScore !== 5) {
+        if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+            (playerSelection === 'scissors' && computerSelection === 'rock') ||
+            (playerSelection === 'paper' && computerSelection === 'scissors')) {
+            infoGraphic.style.visibility = 'visible';
+            infoGraphic.src = './icons/right.png';
+            update.textContent = `ROUND ${round[count]}!`;
+            count += 1;
+            computerScore += 1;
+            computer.textContent = `Computer : ${computerScore}`;
+            if (computerScore === 5) {
+                if (window.confirm(`COMPUTER WON WITH ${computerScore} POINTS!\nDo you want to try again?`)) {
+                    restart();
+                }
+            }
+        }
+        else if (playerSelection === computerSelection) {
+            infoGraphic.style.visibility = 'hidden';
+            update.textContent = `ROUND ${round[count]}!`;
+            count += 1;
 
-
-
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore++;
-
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore++;
-
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++;
-
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore++;
-
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore++;
-
-        } else if (playerSelection === computerSelection) {
-
-        } else {
-            //Tallies Score
-            // (playerScore > computerScore) ?
-            //     window.alert(`Player Won with ${playerScore} points! Congratulations`) :
-            //     window.alert(`Computer Won with ${computerScore} points.! You Suck!`);
-        } 
-            
+        }
+        else {
+            infoGraphic.style.visibility = 'visible';
+            infoGraphic.src = './icons/left.png';
+            update.textContent = `ROUND ${round[count]}!`;
+            count += 1;
+            playerScore += 1;
+            player.textContent = `Player :${playerScore}`;
+            if (playerScore === 5) {
+                if (window.confirm(`PLAYER WON WITH ${playerScore} POINTS!\nDo you want to try again?`)) {
+                    restart();
+                }
+            }
+        }
     }
-
-
-function game() {
-    playRound();
+    else {
+        window.alert('ITS A TIE');
+    }
 }
